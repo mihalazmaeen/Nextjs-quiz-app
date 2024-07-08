6d5c5aa5bfdef9438163839a4eef3b3784097ef3
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-07-08 16:24:52 +0600

    quiz component completed upto timer start

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,11 +1,57 @@
 "use client"
 
 import { useState, useEffect } from "react"
+import StatCard from "./StatCard"
+interface QuizProps{
+    questions:{
+        question:string
+        answers:string[]
+        correctAnswer:string
+    }[]
+    userId:string | undefined
+}
 
+const Quiz = ({questions,userId}:QuizProps) => {
+    const [activeQuestion, setActiveQuestion] = useState(0)
+    const [selectedAnswer, setSelectedAnswer] = useState("")
+    const [checked, setChecked] = useState(false)
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<number|null>(null)
+    const [showResult, setShowResult] = useState(false)
+    const [results, setResults] = useState({
+      score:0,
+      correctAnswers:0,
+      wrongAnswers:0  
+    })
+    const [timeRemaining, setTimeRemaining] = useState(30)
+    const [timerRunning, setTimerRunning] = useState(false)
+    useEffect(()=>{
+        let timer:NodeJS.Timeout
+        if(timerRunning && timeRemaining > 0){
+            timer = setTimeout(()=>{
+                setTimeRemaining((prevTime)=>prevTime - 1)
+            },1000)
+        }else if(timeRemaining === 0){
+            handleTimeUp()
+        }
+        return () => clearTimeout(timer)
+    },[timerRunning,timeRemaining])
 
-const Quiz = () => {
+    const startTimer = ()=>{
+        setTimerRunning(true)
+    }
+    const stopTimer = ()=>{
+        setTimerRunning(false)
+    }
+    const resetTimer = ()=>{
+        setTimeRemaining(30)
+    }
+    const handleTimeUp = ()=>{
+        stopTimer()
+        resetTimer()
+        nextQuestion()
+    }
   return (
-    <div>Quiz</div>
+    <div className="bg-primary text-white px-4 rounded-md py-1">{timeRemaining} seconds to answer </div>
   )
 }
 
